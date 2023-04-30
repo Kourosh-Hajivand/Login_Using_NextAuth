13690942b8af1fffa3f8d2463c7cccb966502865
kourosh-H <kourosh.hajivand0@gmail.com>
2023-04-30 15:09:54 +0330

    changes

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,16 @@
 "use client";
+
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { SessionProvider } from "next-auth/react";
+import Providers from "./providers";
+
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children, session }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider session={session}>{children}</SessionProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+"use client";
 // import Image from "next/image";
-
 import Image from "next/image";
 import PersonalLogo from "/public/logo.png";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 export default function Home() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div className="w-full h-full relative">
       <div className="fixed top-0 left-0 shadow-md w-full h-20 bg-white flex items-center justify-center">
@@ -17,46 +20,68 @@ export default function Home() {
         </Link>
       </div>
       <div className="w-full min-h-screen bg-neutral-200 flex items-center justify-center px-4">
-        <div className="w-full max-w-[500px] h-full bg-white rounded shadow-lg p-5">
-          <div className="text-center font-bold">
-            <h1 className="text-2xl">You are not Loged in</h1>
-            <p className="font-normal text-sm text-neutral-400 my-2">
-              Please login by this option
-            </p>
+        {session ? (
+          <div>
+            <div className="flex items-center justify-center">
+              <Image
+                src={session.user.image}
+                height={180}
+                width={180}
+                className="rounded-full "
+                alt={session.user.email}
+              />
+              <h1 className="text-2xl mx-6">{session.user.name}</h1>
+            </div>
+            <button onClick={() => signOut()}>sign out </button>
           </div>
-          <div className="flex flex-col space-y-2 my-6">
-            <button className="w-full p-3 py-4 bg-blue-500 rounded flex items-center relative hover:bg-blue-600 hover:shadow duration-150">
-              <div className="absolute top-1/2 left-10 transform  -translate-y-1/2">
-                <div className="w-8 h-8 relative">
-                  <Image
-                    src={"https://authjs.dev/img/providers/facebook-dark.svg"}
-                    alt="https://authjs.dev/img/providers/facebook-dark.svg"
-                    fill
-                    className="object-contain"
-                  />
+        ) : (
+          <div className="w-full max-w-[500px] h-full bg-white rounded shadow-lg p-5">
+            <div className="text-center font-bold">
+              <h1 className="text-2xl">You are not Loged in</h1>
+              <p className="font-normal text-sm text-neutral-400 my-2">
+                Please login by this option
+              </p>
+            </div>
+            <div className="flex flex-col space-y-2 my-6">
+              <button
+                onClick={async () => await signIn("facebook")}
+                className="w-full p-3 py-4 bg-blue-500 rounded flex items-center relative hover:bg-blue-600 hover:shadow duration-150"
+              >
+                <div className="absolute top-1/2 left-10 transform  -translate-y-1/2">
+                  <div className="w-8 h-8 relative">
+                    <Image
+                      src={"https://authjs.dev/img/providers/facebook-dark.svg"}
+                      alt="https://authjs.dev/img/providers/facebook-dark.svg"
+                      fill
+                      className="object-contain"
+                    />
+                  </div>
                 </div>
-              </div>
-              <span className="w-full text-center text-white font-semibold">
-                Sign in with Facebook
-              </span>
-            </button>
-            <button className="w-full p-3 py-4 bg-white border-2 rounded flex items-center relative hover:bg-neutral-50 hover:shadow duration-150">
-              <div className="absolute top-1/2 left-10 transform  -translate-y-1/2">
-                <div className="w-8 h-8 relative">
-                  <Image
-                    src={"https://authjs.dev/img/providers/google.svg"}
-                    alt="https://authjs.dev/img/providers/google.svg"
-                    fill
-                    className="object-contain"
-                  />
+                <span className="w-full text-center text-white font-semibold">
+                  Sign in with Facebook
+                </span>
+              </button>
+              <button
+                onClick={async () => await signIn("google")}
+                className="w-full p-3 py-4 bg-white border-2 rounded flex items-center relative hover:bg-neutral-50 hover:shadow duration-150"
+              >
+                <div className="absolute top-1/2 left-10 transform  -translate-y-1/2">
+                  <div className="w-8 h-8 relative">
+                    <Image
+                      src={"https://authjs.dev/img/providers/google.svg"}
+                      alt="https://authjs.dev/img/providers/google.svg"
+                      fill
+                      className="object-contain"
+                    />
+                  </div>
                 </div>
-              </div>
-              <span className="w-full text-center text-neutral-800 font-semibold">
-                Sign in with Google
-              </span>
-            </button>
+                <span className="w-full text-center text-neutral-800 font-semibold">
+                  Sign in with Google
+                </span>
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
